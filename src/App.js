1864f1c9ad2d32d341b3703a999946da7869f154
armeen <armeenfarange@yahoo.com>
2023-05-28 15:25:16 -0700

    Add basic website functionality

--- src/App.js
+++ src/App.js
@@ -3,22 +3,40 @@ import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-do
 import Home from './pages/Home';
 import About from './pages/About';
 import PremiumPage from './pages/PremiumPage';
-
+import { FaGithubSquare } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <nav className="navbar">
+          <div className="navbar-links">
+            <NavLink className="navlink" exact activeClassName="active" to="/">
+              Home
+            </NavLink>
+            <NavLink className="navlink" activeClassName="active" to="about">
+              About
+            </NavLink>
+            <NavLink className="navlink" activeClassName="active" to="premium">
+              AWS Auth
+            </NavLink>
+          </div>
+          <a
+            className="social-link"
+            href="https://github.com/ArmeenF"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithubSquare  />
+          </a>
+        </nav>
         <div className="content">
-          <NavLink className="content" exact activeClassName="active" to="/">Home</NavLink>
-          <NavLink className="content" activeClassName="active" to="about">About</NavLink>
-          <NavLink className="content" activeClassName="active" to="premium">Premium Content</NavLink>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/premium" element={<PremiumPage />} />
+          </Routes>
         </div>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/premium" element={<PremiumPage />}></Route>
-        </Routes>
       </Router>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import {Amplify} from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="app-container">
+      <App />
+    </div>
   </React.StrictMode>
 );
 
